af12104e199b2d4969996a146d9c913c2cefc462
LabElec <labelec@mac-labelec-02.42.fr>
2017-05-16 00:21:48 +0200

    Add export c file

--- xen_compiler.py
+++ xen_compiler.py
@@ -2,29 +2,33 @@ import sys
 import os.path
 
 if len(sys.argv) <= 1:
-	print "Usage: python xen_compiler.py [xenfile to compile]"
+	print "Usage: python xen_compiler.py [xenfile to compile] {-c to gen c var}"
 	sys.exit(0)
 
 if not os.path.exists(sys.argv[1]):
 	print "Fichier introuvable.";
 	sys.exit(0)
 
+if len(sys.argv) >= 2 and sys.argv[2] == "-c":
+	tocvar = 1
+
 try:
 	with open(sys.argv[1]) as file:
 		iline = 1
 		bytesbuff = bytearray()
+		cbuff = "unsigned char data[] = {\n"
 		for line in file:
 			line = line.replace('\n', '').replace('\r', '')
 			split = line.split(';')
 			if len(split) != 4:
 				print "Fichier " + sys.argv[1] + ", ligne " + str(iline) + '\n' + "Erreur de syntaxe: ';' indefini"
 				sys.exit(0)
-			
+
 			split[0] = int(split[0])
 			split[1] = int(split[1])
 			split[2] = int(split[2])
 			split[3] = int(split[3])
-		
+
 			# Verification des valeurs
 			if split[0] < 0 or split[0] > 5:
 				print "Fichier " + sys.argv[1] + ", ligne " + str(iline) + '\n' + "Erreur de syntaxe: Octave incorrect"
@@ -40,17 +44,27 @@ try:
 				sys.exit(0)
 
 			#Ecriture du buffer
-			bytesbuff.append(split[0])
-			bytesbuff.append(split[1])
-			bytesbuff.append(split[2])
-			bytesbuff.append(split[3])
+			if tocvar:
+				cbuff += "\t" + str(split[0]) + ", " + str(split[1]) + ", " + str(split[2]) + ", " + str(split[3]) + ",\n"
+			else:
+				bytesbuff.append(split[0])
+				bytesbuff.append(split[1])
+				bytesbuff.append(split[2])
+				bytesbuff.append(split[3])
 			iline = iline + 1
 
 		#Ecriture du fichier binaire
-		filebin = sys.argv[1].split('.')[0] + '.bin'
-		with open(filebin, "wb") as file:
-			file.write(bytesbuff)
-		print "Compilation terminee. " + sys.argv[1] + " -> " + filebin
+		if tocvar:
+			cbuff = cbuff[0:len(cbuff) - 2] + "\n};"
+			filec = sys.argv[1].split('.')[0] + '.c'
+			with open(filec, "w") as file:
+				file.write(cbuff)
+			print "Compilation terminee. " + sys.argv[1] + " -> " + filec
+		else:
+			filebin = sys.argv[1].split('.')[0] + '.bin'
+			with open(filebin, "wb") as file:
+				file.write(bytesbuff)
+			print "Compilation terminee. " + sys.argv[1] + " -> " + filebin
 except IOError as err:
 	print "Erreur fichier: " + err.strerror
 	sys.exit(0)
